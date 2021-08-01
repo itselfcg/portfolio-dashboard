ac423287a31093785721c04884264d8c91825b6f
Itzel Contreras <itselfcg@gmail.com>
2021-08-01 14:02:10 -0700

    heroku configs

---
+++ server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const app = express();
+
+// Serve static files
+app.use(express.static(__dirname + '/dist/dashboard'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/dashboard/index.html'));
+});
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
